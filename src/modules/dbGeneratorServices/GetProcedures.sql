83489a7f41c7e5ce5507fef53a9c28d4afd4c0fb
bknprofi@lugovov.ru <git@lugovov.ru>
2022-07-13 12:46:48 +0300

    приведениe кода

--- src/modules/dbGeneratorServices/GetProcedures.sql
+++ src/modules/dbGeneratorServices/GetProcedures.sql
@@ -1,30 +1,27 @@
 Select
     convert(concat_ws("",  (SELECT concat(
-                                           "DROP ",type," IF EXISTS `DB_NAME`.`",specific_Name,"`;
-
-CREATE DEFINER=`semen`@`%` ", type," `DB_NAME`.`", specific_Name
-                                       , concat("`("
-                                               , param_list
-                                               , ") "
-                                               ,
-                                                CASE
-                                                    WHEN type = "FUNCTION" THEN
-                                                        concat("\nRETURNS ", RETURNS)
-                                                    ELSE
-                                                        ""
-                                                    END,
-                                                "
-                                   LANGUAGE ",p.language,
-                                                IF(p.is_deterministic="NO","
-NOT ","
-"),"DETERMINISTIC",
-                                                replace(sql_data_access,"_"," "),
-                                                "
+"DROP ",type," IF EXISTS `DB_NAME`.`",specific_Name,"`;
+CREATE ",p.security_type,"=",p.definer," ", type," `DB_NAME`.`", specific_Name
+    , concat("`("
+            , param_list
+            , ") "
+            ,
+            CASE
+                WHEN type = "FUNCTION" THEN
+                    concat("\nRETURNS ", RETURNS)
+                ELSE
+                    ""
+                END,
+            "
+    LANGUAGE ",p.language,
+    IF(p.is_deterministic="NO"," NOT ",""),"DETERMINISTIC
+    ",replace(sql_data_access,"_"," "),
+    "
                           SQL SECURITY INVOKER
                           ", body)
                                        ,";
 
-	DELETE FROM `DB_NAME`.`s_procparams` WHERE procName=", quote(specific_Name),";
+DELETE FROM `DB_NAME`.`s_procparams` WHERE procName=", quote(specific_Name),";
 
 	" ) as text
                             FROM
@@ -32,7 +29,7 @@ NOT ","
                             WHERE
                                     db="DB_NAME" and name=p2.name LIMIT 1)
 	,
-	(    select concat("INSERT INTO DB_NAME.`s_procparams` (procName,name,type,valueMin,valueMax,length,required,`default`) VALUES
+	(    select concat("INSERT INTO `DB_NAME`.`s_procparams` (procName,name,type,valueMin,valueMax,length,required,`default`) VALUES
 	",group_concat(concat(" (",quote(procName),",",quote(name),",",quote(type),",",quote(valueMin),",",quote(valueMax),",",quote(length),",",quote(required)
 	,",",quote(`default`),")") separator ",
 	"),";") from `proc`.s_procparams pp where pp.procName=p2.name),"
